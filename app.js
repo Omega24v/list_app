4cc9fd33a542ec9163314ab3d2be4285cfe3fa00
Anton <omega24v1993@gmail.com>
2020-07-15 00:25:43 +0300

    add new function check to select options length

--- app.js
+++ app.js
@@ -4,38 +4,32 @@ const insertInput = document.querySelector('.insertInput');
 const clearItem = document.querySelector('.clear_item');
 const deleteBtn = document.querySelector('.delete_item');
 
+// Disabled Buttons
+clearItem.disabled = true
+deleteBtn.disabled = true
+
 // Function Add Item To List 
 const addItemList = () => {   
     insertInput.addEventListener('keypress', event => {
         if (event.key === 'Enter') {
-            let inputValue = insertInput.value,
-                newOption = new Option(inputValue, inputValue);   
+            let inputValue  = insertInput.value,
+                newOption   = new Option(inputValue, inputValue);   
 
             listCollection.append(newOption);
             insertInput.value = '';
 
+            checkIsHasOption()
             clearList()
         }
     })
 }
 addItemList()
 
-// Clear List Function 
-const clearList = () => {
-    // Check is Clear Button Ebable
-    (listCollection.options.length >= 1) ? clearItem.disabled = false : clearItem.disabled = true;
-
-    clearItem.addEventListener('click', event => {
-        listCollection.options.length = 0;
-    })
-}
-clearList()
-
 // Take Selected Option from Select
 const selectedOptions = () => {
     let selectedItem = listCollection.selectedOptions;
     
-    listCollection.addEventListener('change', event => {
+    listCollection.addEventListener('change', () => {
         for (let i = 0; i < selectedItem.length; i++) {
             let selected = selectedItem[i];
             deleteItem(selected)
@@ -44,9 +38,33 @@ const selectedOptions = () => {
 }
 selectedOptions()
 
+// Check is select has option
+const checkIsHasOption = () => {
+    if (listCollection.options.length >= 1) {
+        clearItem.disabled = false 
+        deleteBtn.disabled = false
+    } else {
+        clearItem.disabled = true
+        deleteBtn.disabled = true
+    }
+}
+checkIsHasOption()
+
+// Clear List Function 
+const clearList = () => {
+    // Check is Clear Button Ebable
+    clearItem.addEventListener('click', () => {
+        listCollection.options.length = 0;
+        checkIsHasOption()
+    })
+}
+clearList()
+
 // Delete Option from Select
 const deleteItem = (selectedItems) => {
-    deleteBtn.addEventListener('click', event => {
+
+    deleteBtn.addEventListener('click', () => {
         selectedItems.remove();
+        checkIsHasOption()
     })
 }
